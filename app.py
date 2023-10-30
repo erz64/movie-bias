d669b1f1e53f64d0f8002efb0a13500960c34ab3
erz64 <101719207+erz64@users.noreply.github.com>
2023-10-30 16:24:42 +0200

    added a new route for new UserPlots

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("frontpage.html", items=ScoreHelper.get_movie_titles(), scores=ScoreHelper.get_happiness_scores())
+
+@app.route('/additional_plots')
+def additional_plots():
+    return render_template("additional_plots.html")
